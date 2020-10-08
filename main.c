61a7b42a00910ac7bc817a33715f96f191d9b8af
Nagy Dominik <dominik.nagy@irfsolutions.at>
2020-10-08 13:05:14 +0200

    Attempt to color histogram equazation.

--- main.c
+++ main.c
@@ -5,9 +5,27 @@
 
 typedef struct _s_hist_elem hist_elem;
 typedef struct _s_kep t_kep;
+typedef struct _s_rgb t_rgb;
+typedef struct _s_ycrcb t_ycrcb;
 typedef struct _s_hist histogram;
 struct _s_kep {
   unsigned char *tomb;
+  size_t szelesseg;
+  size_t magassag;
+  size_t elemszam;
+};
+struct _s_rgb {
+  unsigned char *r;
+  unsigned char *g;
+  unsigned char *b;
+  size_t szelesseg;
+  size_t magassag;
+  size_t elemszam;
+};
+struct _s_ycrcb {
+  unsigned char *y;
+  unsigned char *cr;
+  unsigned char *cb;
   size_t szelesseg;
   size_t magassag;
   size_t elemszam;
@@ -28,6 +46,13 @@ void kumul_eloszlas(histogram *, histogram *);
 void uj_ertek(histogram *, histogram *, size_t);
 void normalize(t_kep *, t_kep *, histogram *);
 void kiiras(t_kep *, char *);
+void _convert_RGbtoYCrCb(t_rgb *, t_ycrcb *);
+unsigned char _calc_Y(size_t, size_t, size_t);
+unsigned char _calc_Cb(size_t, size_t, size_t);
+unsigned char _calc_Cr(size_t, size_t, size_t);
+unsigned char _calc_R(size_t y, size_t cb, size_t cr);
+unsigned char _calc_G(size_t y, size_t cb, size_t cr);
+unsigned char _calc_B(size_t y, size_t cb, size_t cr);
 
 int main(int argc, char *argv[]) {
   if (argc < 2)
@@ -182,3 +207,111 @@ void kiiras(t_kep *kep, char *fajlnev) {
   }
   fclose(fp);
 }
+
+/** Convert RGB to YCrCb
+ *
+ * Converts 8bit integer RGB values to YCrCb colorspace.
+ */
+void _convert_RGbtoYCrCb(t_rgb *r, t_ycrcb *y) {
+  free(y->y);
+  free(y->cb);
+  free(y->cr);
+  y->elemszam = r->elemszam;
+  y->szelesseg = r->szelesseg;
+  y->magassag = r->magassag;
+
+  y->y = (unsigned char *)malloc(sizeof(unsigned char *) * y->elemszam);
+  y->cb = (unsigned char *)malloc(sizeof(unsigned char *) * y->elemszam);
+  y->cr = (unsigned char *)malloc(sizeof(unsigned char *) * y->elemszam);
+
+  for(size_t i=0; i < r->elemszam; i++) {
+   y->y[i]  = _calc_Y(r->r[i], r->g[i], r->b[i]);
+   y->cb[i] = _calc_Y(r->r[i], r->g[i], r->b[i]);
+   y->cr[i] = _calc_Y(r->r[i], r->g[i], r->b[i]);
+  }
+}
+void _convert_YCrCbtoRGB(t_ycrcb *y, t_rgb *r) {
+  free(r->r);
+  free(r->b);
+  free(r->g);
+  r->elemszam = y->elemszam;
+  r->szelesseg = y->szelesseg;
+  r->magassag = y->magassag;
+
+  r->r = (unsigned char *)malloc(sizeof(unsigned char *) * y->elemszam);
+  r->g = (unsigned char *)malloc(sizeof(unsigned char *) * y->elemszam);
+  r->b = (unsigned char *)malloc(sizeof(unsigned char *) * y->elemszam);
+
+  for(size_t i=0; i < r->elemszam; i++) {
+   r->r[i] = _calc_R(y->y[i], y->cb[i], y->cr[i]);
+   r->g[i] = _calc_G(y->y[i], y->cb[i], y->cr[i]);
+   r->b[i] = _calc_B(y->y[i], y->cb[i], y->cr[i]);
+  }
+}
+
+void normalize_RGB(t_rgb *kep) {
+  t_ycrcb Y;
+  _convert_RGbtoYCrCb(kep, &Y);
+  histogram Y_hist, Y_elosz, Y_uj;
+  t_kep Y_kep, Y_kep_uj;
+  Y_kep.elemszam = kep->elemszam;
+  Y_kep.szelesseg = kep->szelesseg;
+  Y_kep.magassag = kep->magassag;
+  Y_kep.tomb = (unsigned char *)malloc(sizeof(unsigned char)*kep->elemszam);
+  for(size_t i=0; i < kep->elemszam; i++) {
+    Y_kep.tomb[i] = Y.y[i];
+  }
+
+  hisztogram_keszit( &Y_kep, &Y_hist, 256);
+  kumul_eloszlas(&Y_hist, &Y_elosz);
+  uj_ertek(&Y_elosz, &Y_uj, Y_kep.elemszam);
+  normalize(&Y_kep, &Y_kep_uj, &Y_uj);
+
+  for(size_t i=0; i < kep->elemszam; i++) {
+    Y.y[i] = Y_kep_uj.tomb[i];
+  }
+  _convert_YCrCbtoRGB( &Y, kep);
+  free(Y.cb);free(Y.cr);free(Y.y);
+  free(Y_hist.tomb); free(Y_elosz.tomb); free(Y_uj.tomb);
+  free(Y_kep.tomb); free(Y_kep_uj.tomb);
+}
+
+
+
+unsigned char _calc_Y(size_t r, size_t g, size_t b) {
+  unsigned char _r = ((unsigned int)(65.738 * (r/256))) >> 8;
+  unsigned char _g = ((unsigned int)(129.057 * (r/256))) >> 8;
+  unsigned char _b = ((unsigned int)(25.064 * (r/256))) >> 8;
+  return 16 + _r + _g + _b;
+}
+
+unsigned char _calc_Cb(size_t r, size_t g, size_t b) {
+  unsigned char _r = ((unsigned int)(37.945 * (r/256))) >> 8;
+  unsigned char _g = ((unsigned int)(74.494 * (r/256))) >> 8;
+  unsigned char _b = ((unsigned int)(112.439 * (r/256))) >> 8;
+  return 128 - _r - _g + _b;
+}
+unsigned char _calc_Cr(size_t r, size_t g, size_t b) {
+  unsigned char _r = ((unsigned int)(112.439 * (r/256))) >> 8;
+  unsigned char _g = ((unsigned int)(94.154 * (r/256))) >> 8;
+  unsigned char _b = ((unsigned int)(18.285 * (r/256))) >> 8;
+  return 128 + _r - _g - _b;
+}
+
+unsigned char _calc_R(size_t y, size_t cb, size_t cr) {
+  unsigned char _y = ((unsigned int)(298.082 * y)) >> 8;
+  unsigned char _cr = ((unsigned int)(408.583 * cr)) >> 8;
+  return _y + _cr - 222.921;
+}
+unsigned char _calc_G(size_t y, size_t cb, size_t cr) {
+  unsigned char _y = ((unsigned int)(298.082 * y)) >> 8;
+  unsigned char _cb = ((unsigned int)(100.291 * cb)) >> 8;
+  unsigned char _cr = ((unsigned int)(208.120 * cr)) >> 8;
+  return _y - _cb - _cr + 135.576;
+}
+unsigned char _calc_B(size_t y, size_t cb, size_t cr) {
+  unsigned char _y = ((unsigned int)(298.082 * y)) >> 8;
+  unsigned char _cb = ((unsigned int)(516.412 * cb)) >> 8;
+  return _y + _cb - 276.863;
+}
+
